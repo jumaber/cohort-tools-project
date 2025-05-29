ff33695d9e37b93706335100e9e215adaab62e9e
FabioMiranda-123 <fabio_rebelo@outlook.pt>
2025-05-29 19:40:00 +0100

    Install mongoose & Create a DB

--- server/app.js
+++ server/app.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const PORT = 5005;
+const cors = require("cors");
+const mongoose = require('mongoose');
 
-// STATIC DATA
+
+
+// DYNAMIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-const cohorts = require("./public/data/cohorts.json");
-const students = require("./public/data/students.json");
+const Cohort = require("./models/Cohort.js");
+const Student = require("./models/Student.js");
 
+// DEFINE PORT
+const PORT = 5005;
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
+// MONGOOSE 
+mongoose.connect("mongodb://localhost/cohort-tools-api");
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -21,7 +28,13 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5005"
+    ],
+  })
+);
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -31,11 +44,15 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
+  Cohort.find()
+  .then(cohorts => res.json(cohorts))
+  .catch(err => res.status(500).json({ message: "Error fetching cohorts 😬", error: err }))
 });
 
 app.get("/api/students", (req, res) => {
-  res.json(students);
+  Student.find()
+  .then(students => res.json(students))
+  .catch(err => res.status(500).json({ message: "Error fetching cohorts 😬", error: err }))
 });
 
 
@@ -43,3 +60,5 @@ app.get("/api/students", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+
